f4ee63bceb126e3ef95866e2b586fe40dfc3204b
andreseichi <andre_seichi@hotmail.com>
2022-06-14 22:31:02 -0300

    consuming the api route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,27 @@
-import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 
-const Home: NextPage = () => {
+import { api } from '../services/api';
+
+import { Holidays } from '../types/holidays';
+
+export default function Home() {
+  const [holidays, setHolidays] = useState<Holidays[]>([]);
+
+  useEffect(() => {
+    getHolidays();
+  }, []);
+
+  async function getHolidays() {
+    try {
+      const { data } = await api.get('holidays');
+
+      setHolidays(data);
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return <h1>hello world</h1>;
-};
-
-export default Home;
+}
